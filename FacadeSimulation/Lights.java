521c9b824e93326ffb28867e95f7eb9eaad968d2
Wilbert Diaz <112036842+WilbertDiaz@users.noreply.github.com>
2025-12-08 18:55:11 -0600

    Add files via upload

---
+++ FacadeSimulation/Lights.java
@@ -0,0 +1,9 @@
+public class Lights {
+    public void dim(int level) {
+        System.out.println("Luces atenuadas al " + level + "%.");
+    }
+    
+    public void on() {
+        System.out.println("Luces encendidas.");
+    }
+}
