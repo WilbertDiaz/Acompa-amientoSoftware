6b3b3e3655cecc5661923de9417c08eccc9828e8
Wilbert Diaz <112036842+WilbertDiaz@users.noreply.github.com>
2025-12-11 12:00:14 -0600

    Delete FacadeSimulation/Lights.java

--- FacadeSimulation/Lights.java
+++
@@ -1,9 +0,0 @@
-public class Lights {
-    public void dim(int level) {
-        System.out.println("Luces atenuadas al " + level + "%.");
-    }
-    
-    public void on() {
-        System.out.println("Luces encendidas.");
-    }
-}
